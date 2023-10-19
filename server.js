eb2c4be8a7f675a25ef7ac633996f87ba3438e2b
Abhishek Rai <rssabhishek4@gmail.com>
2023-10-19 16:47:19 +0530

    folderStructure

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require ("express");
 const app = express();
 
 app.get("/",(req,res)=>{
-    res.send("api is working fine node is working  ")
+    res.send("api is working fine node is sfbskjbfdkjsworking  ")
 })
 
 app.listen(3000,()=>{
-    console.log(`api is working fine on 3000` )
+    console.log(`api is working fine on 30009888` )
 })
