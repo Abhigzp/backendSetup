97691d52212b5612f80a48c0b5f3cc187034af80
Abhishek rai <rssabhishek4@gmail.com>
2023-10-19 19:45:59 +0530

    correct login

---
+++ app/controllers/commonApi/userController.js
@@ -0,0 +1,90 @@
+const User = require('../../models/userModels');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const secretKey = 'yourSecretKey'; // Replace with a strong, secret key
+
+const userRegister = async (req, res) => {
+  const { name, email, phone, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(401).json({ message: 'Email already registered' });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      name,
+      email,
+      phone,
+      password: hashedPassword
+    });
+    const savedUser = await newUser.save();
+    const responseUser = {
+      name: savedUser.name,
+      email: savedUser.email
+    };
+
+    res.send({ status: 'success', code: 200, message: "User registration successfully", user: responseUser });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+    const userWithoutPassword = {
+      _id: user._id,
+      email: user.email,
+    };
+    const token = jwt.sign({ user: userWithoutPassword }, secretKey, { expiresIn: '1h' }); // You can adjust the expiration time as needed
+
+    res.send({ status: 'success', code: 200, message: 'Login successfully', token, userData: userWithoutPassword });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const getAllUsers = async (req, res) => {
+  try {
+    User.find()
+      .then((results) => {
+        res.send({ status: 'success', code: 200, message: "users data", users: results });
+      })
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
+
+const getUserByID = async (req, res) => {
+  try {
+    const { _id } = req.body;
+    User.findOne({ _id }).then((results) => {
+      res.send({ status: 'success', code: 200, message: "user data", users: results });
+    })
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
+
+
+const updateUser = async (req, res) => {
+  try {
+
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
+
+
+module.exports = { userRegister, login, getAllUsers, getUserByID, updateUser };
--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-const express = require ("express");
+const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyparser = require('body-parser');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+const ApiRoute = require('./app/routes/userRoutes')
 
-app.get("/",(req,res)=>{
-    res.send("api is working fine node is sfbskjbfdkjsworking  ")
-})
+mongoose.connect('mongodb://127.0.0.1:27017/appdb')
+  .then(() => {
+    console.log('data base Connected!')
+  } )
+  .catch((error)=>{
+    console.log(`someissues in database,${error}`)
+  })
 
-app.listen(3000,()=>{
-    console.log(`api is working fine on 30009888` )
+app.use(cors());
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json())
+app.use('/api/v1', ApiRoute);
+
+app.listen(PORT,()=>{
+  console.log(`my app is running in this port ${PORT}`)
 })
