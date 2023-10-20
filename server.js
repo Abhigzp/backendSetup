94394a6a66262d78da0fefcda38af387ba22fdb9
Abhishek rai <rssabhishek4@gmail.com>
2023-10-20 23:21:18 +0530

    some code behaviour

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
 const cors = require('cors');
 const bodyparser = require('body-parser');
 const path = require('path');
 require('./app/config/database.config');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.NODE_PORT;
 const ApiRoute = require('./app/routes/userRoutes')
 
 
@@ -13,6 +14,8 @@ app.use(cors());
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json())
 app.use('/api/v1', ApiRoute);
+
+
 
 app.listen(PORT,()=>{
   console.log(`my app is running in this port ${PORT}`)
